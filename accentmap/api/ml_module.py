62dd05d444bd492e03fffc2941580b47c52d9686
Cianán <c22393223@mytudublin.ie>
2025-12-03 10:50:09 +0000

    Add comments and clean up in ml_module and urls.py
    Added explanatory comments to the ML module for clarity on MFCC extraction and model loading. Removed boilerplate comments from urls.py to streamline the file.

--- accentmap/api/ml_module.py
+++ accentmap/api/ml_module.py
@@ -3,6 +3,7 @@ import numpy as np
 import joblib
 import os
 
+# dynamically alligns filepath so future machines dont break it
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "rf_model.pkl")
 model = joblib.load(MODEL_PATH)
 
@@ -14,11 +15,15 @@ REGION_TO_COORDS = {
 }
 
 def extract_mfcc(path):
+    # y = audio data (array of amplitudes).  sr = sample rate
     y, sr = librosa.load(path, sr=16000, mono=True)
+    # Mel-frequency cepstral coefficients
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
     return np.mean(mfcc.T, axis=0)
+    # “Because audio clips vary in length, I average the MFCCs over time so every clip becomes a fixed-length feature vector.”
 
 def predict(path):
+    # 2d array for scikit
     features = extract_mfcc(path).reshape(1, -1)
     label = model.predict(features)[0]           # e.g. "Leinster"
     coords = REGION_TO_COORDS.get(label, (53.4, -8.2))
